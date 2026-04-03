b380582e3f5e949c0ff30973ff03b2907d777992
joguel63 <80803870+joguel63@users.noreply.github.com>
2026-04-03 18:38:12 -0500

    fix: refine InputProps type definition for control

--- src/core/types/inputProps.type.ts
+++ src/core/types/inputProps.type.ts
@@ -2,5 +2,5 @@ import { TextFieldProps } from '@mui/material'
 import { Control, FieldValues } from 'react-hook-form'
 
 export type InputProps = TextFieldProps & {
-  control: Control<FieldValues, any>
+  control: Control<FieldValues>
 }
