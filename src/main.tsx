f9bb965e4939ded6a007a83c82dfd24b9b6e564d
joguel63 <80803870+joguel63@users.noreply.github.com>
2026-04-04 22:16:20 -0500

    feat: remove index.css import from main.tsx to streamline styling management

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
+import 'core/locales'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import 'core/locales'
 import App from './App.tsx'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
